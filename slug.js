e823af6fccf78b13eda9840d8685dec56b78ee5d
Ayo-Oluwa Adebayo <ayo-oluwaadebayo@Ayo-Oluwas-MacBook-Pro.local>
2019-03-03 08:27:56 +0100

    [refactor code]

--- slug.js
+++ slug.js
@@ -3,3 +3,15 @@ function getURLSlug(words) {
     .replace(/\s+/g, '-')
     .toLowerCase();
 }
+
+function getInitials() {
+  return name.split(' ')
+    .map(word => word.charAt(0).toUpperCase())
+    .join('');
+}
+
+function getInitials() {
+  return name.split('')
+    .map(word => word.charAt(0).toUpperCase())
+    .join(' ');
+}
